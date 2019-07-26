e948e14e065623626eb4b06bc8103157774b4f9e
salt-die <naclmolecule@gmail.com>
2019-07-26 18:51:56 -0500

    Cleaning up variables; more PEP conformity

--- soap.py
+++ soap.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+soap: a useless experiment in pygame
+
+Poke dynamic voronoi cells by left-clicking or create new cells by right-
+clicking.
+"""
+from numpy import pi, array, where, sin, clip
+from numpy.linalg import norm
+from numpy.random import random_sample
+from scipy.spatial import Voronoi
+from scipy.spatial.qhull import QhullError
 import pygame
 import pygame.freetype #For loading font
 from pygame.draw import polygon, aalines, circle
-from numpy import pi, array, where, sin, clip
-from numpy.linalg import norm
-from random import randint
-from scipy.spatial import Voronoi
-from scipy.spatial.qhull import QhullError
 
 class Center:
     """
@@ -24,6 +30,15 @@ def game():
     The main game functions, constants, and variables, along with the main
     while loop.
     """
+    def render_help():
+        """
+        Renders help_text
+        """
+        nonlocal help_text
+        font = pygame.freetype.Font('NotoSansMono-Regular.ttf', 20)
+        help_text = [font.render(text, (255, 255, 255)) for text in help_text]
+        help_text = [i for i, _ in help_text] #Throw away the rect-like tuples
+        
     #Game Functions-----------------------------------------------------------
     def move_centers():
         """
@@ -42,7 +57,7 @@ def game():
             fric = .97
             velocity = fric * velocity
             velocity[abs(velocity) < .01] = 0.0         #Prevent jitter
-            clip(velocity, -MAX_VEL, MAX_VEL, velocity) #Prevent OOB
+            clip(velocity, -max_vel, max_vel, velocity) #Prevent OOB
             return velocity
 
         def decel_and_move(center):
@@ -54,33 +69,37 @@ def game():
             center.loc += center.velocity
 
         #Movement for cell centers
-        if BOUNCING:
+        if booleans_dict["bouncing"]:
             for center in centers:
                 #Reverse the velocity if out-of-bounds
-                center.velocity[(center.loc < MAX_VEL)|\
-                                (center.loc > OOB_DIM - MAX_VEL)] *= -1
+                center.velocity[(center.loc < max_vel)|\
+                                (center.loc > window_dim - max_vel)] *= -1
                 decel_and_move(center)
         else: #Delete out-of-bound centers
             oob = {center for center in centers\
-                   if ((0 > center.loc)|(center.loc > OOB_DIM)).any()}
+                   if ((center.loc < 0)|(window_dim < center.loc)).any()}
             centers.difference_update(oob)
-            for center in centers: decel_and_move(center)
+            for center in centers:
+                decel_and_move(center)
 
         #Movement for color center
-        if not (UP or DOWN or LEFT or RIGHT):
+        if not (booleans_dict["up"] or\
+                booleans_dict["down"] or\
+                booleans_dict["left"] or\
+                booleans_dict["right"]):
             decel_and_move(color_center)
         else:
-            if UP and color_center.velocity[1] > -MAX_VEL:
+            if booleans_dict["up"] and color_center.velocity[1] > -max_vel:
                 color_center.velocity[1] -= .5
-            if DOWN and color_center.velocity[1] < MAX_VEL:
+            if booleans_dict["down"] and color_center.velocity[1] < max_vel:
                 color_center.velocity[1] += .5
-            if LEFT and color_center.velocity[0] > -MAX_VEL:
+            if booleans_dict["left"] and color_center.velocity[0] > -max_vel:
                 color_center.velocity[0] -= .5
-            if RIGHT and color_center.velocity[0] < MAX_VEL:
+            if booleans_dict["right"] and color_center.velocity[0] < max_vel:
                 color_center.velocity[0] += .5
             color_center.loc += color_center.velocity
         #"Wrap" around the screen instead of heading out-of-bounds
-        color_center.loc %= OOB_DIM
+        color_center.loc %= window_dim
 
     def draw_voronoi_cells():
         """
@@ -102,7 +121,7 @@ def game():
             distance = norm(color_center.loc - point)
             color = (127 * sin(frequency * distance +\
                                array((0, 2*pi/3, 4*pi/3))) + 128).astype(int)
-            return PALETTE(color)
+            return palette(color)
 
         points = [center.loc for center in centers]
         #Comment out if you'd prefer no voronoi cell around the color center.
@@ -130,22 +149,22 @@ def game():
                     for i, reg in enumerate(vor.regions)\
                     if len(reg) > 3 or (len(reg) == 3 and -1 not in reg)]
 
-        if FILL:
+        if booleans_dict["fill"]:
             for center, poly in polygons:
-                polygon(WINDOW, get_color(center), poly)
+                polygon(window, get_color(center), poly)
 
-        if OUTLINE:
+        if booleans_dict["outline"]:
             for _, poly in polygons:
-                aalines(WINDOW, (255, 255, 255), True, poly, 1)
+                aalines(window, (255, 255, 255), True, poly, 1)
 
     def draw_centers():
         """
         Draws a tiny circle at each center location.
         """
         for center in centers:
-            circle(WINDOW, (255, 255, 255), center.loc.astype(int), 3)
+            circle(window, (255, 255, 255), center.loc.astype(int), 3)
 
-        circle(WINDOW, (0, 0, 0), color_center.loc.astype(int), 5)
+        circle(window, (0, 0, 0), color_center.loc.astype(int), 5)
 
     def draw_help():
         """
@@ -154,11 +173,10 @@ def game():
         help_background = pygame.Surface((670, 350))
         help_background.set_alpha(140)
         help_background.fill((0, 0, 0))
-        help_coordinates = array([(WINDOW_WIDTH-670)//2,\
-                                  ((WINDOW_HEIGHT-350)//2)])
-        WINDOW.blit(help_background, help_coordinates)
+        help_coordinates = (window_dim - array([670.0, 350.0])) // 2
+        window.blit(help_background, help_coordinates)
         for i, line in enumerate(help_text):
-            WINDOW.blit(line, help_coordinates+array([25, 16 + 25 * i]))
+            window.blit(line, help_coordinates+array([25, 16 + 25 * i]))
 
     #INPUT FUNCTIONS----------------------------------------------------------
     def get_user_input():
@@ -168,21 +186,21 @@ def game():
         wasd = {97, 115, 100, 119}
         for event in pygame.event.get():
             if event.type == 12: #Quit
-                nonlocal running
-                running = False
-            elif event.type == 2: #Key down
-                KEYDOWN_dict.get(event.key, no_key)()
+                nonlocal booleans_dict
+                booleans_dict["running"] = False
+            elif event.type == 2: #key down
+                keydown_dict.get(event.key, no_key)()
                 if event.key in wasd:
                     color_move_start(event.key)
                 elif event.key == 32: #space
                     poke(color_center.loc)
-            elif event.type == 3: #Key up
+            elif event.type == 3: #key up
                 if event.key in wasd:
                     color_move_stop(event.key)
             elif event.type == 5: #Mouse down
-                if event.button == 1:   #Left-Click
+                if event.button == 1:   #left-Click
                     poke(array(pygame.mouse.get_pos()))
-                elif event.button == 3: #Right-Click
+                elif event.button == 3: #right-Click
                     centers.add(Center(array(pygame.mouse.get_pos())\
                                        .astype(float), array([0.0, 0.0])))
 
@@ -191,53 +209,52 @@ def game():
         Reset position of the cell centers.
         """
         nonlocal centers
-        centers = {Center(array([randint(MAX_VEL, WINDOW_WIDTH - MAX_VEL),\
-                                 randint(MAX_VEL, WINDOW_HEIGHT - MAX_VEL)])\
-                          .astype(float), array([0.0, 0.0]))\
+        centers = {Center(random_sample(2) * (window_dim - max_vel),\
+                          array([0.0, 0.0]))\
                    for i in range(number_of_centers)}
 
     def toggle_centers():
         """
         Toggle showing cell centers.
         """
-        nonlocal CENTERS_VISIBLE
-        CENTERS_VISIBLE = not CENTERS_VISIBLE
+        nonlocal booleans_dict
+        booleans_dict["centers_visible"] = not booleans_dict["centers_visible"]
 
     def toggle_bouncing():
         """
         Toggle bouncing off boundaries. If off, out-of-bound centers are
         deleted.
         """
-        nonlocal BOUNCING
-        BOUNCING = not BOUNCING
+        nonlocal booleans_dict
+        booleans_dict["bouncing"] = not booleans_dict["bouncing"]
 
     def toggle_fill():
         """
         Toggle coloring the Voronoi cells.
         """
-        nonlocal FILL
-        FILL = not FILL
+        nonlocal booleans_dict
+        booleans_dict["fill"] = not booleans_dict["fill"]
 
     def toggle_outline():
         """
         Toggle drawing the outling of the Voronoi cells.
         """
-        nonlocal OUTLINE
-        OUTLINE = not OUTLINE
+        nonlocal booleans_dict
+        booleans_dict["outline"] = not booleans_dict["outline"]
 
     def next_palette():
         """
-        Change the colors used to color Voronoi cells.
+        Change the colors used to fill Voronoi cells.
         """
-        nonlocal PALETTE
-        PALETTE = palettes[(palettes.index(PALETTE) + 1) % len(palettes)]
+        nonlocal palette
+        palette = palettes[(palettes.index(palette) + 1) % len(palettes)]
 
     def toggle_help():
         """
         Show the help menu.
         """
-        nonlocal HELP
-        HELP = not HELP
+        nonlocal booleans_dict
+        booleans_dict["show_help"] = not booleans_dict["show_help"]
 
     def no_key():
         """
@@ -249,35 +266,29 @@ def game():
         """
         Start moving the color center in the corresponding direction.
         """
+        nonlocal booleans_dict
         if key == 119:
-            nonlocal UP
-            UP = True
+            booleans_dict["up"] = True
         elif key == 97:
-            nonlocal LEFT
-            LEFT = True
+            booleans_dict["left"] = True
         elif key == 115:
-            nonlocal DOWN
-            DOWN = True
+            booleans_dict["down"] = True
         elif key == 100:
-            nonlocal RIGHT
-            RIGHT = True
+            booleans_dict["right"] = True
 
     def color_move_stop(key):
         """
         Stop moving the color center in the corresponding direction.
         """
+        nonlocal booleans_dict
         if key == 119:
-            nonlocal UP
-            UP = False
+            booleans_dict["up"] = False
         elif key == 97:
-            nonlocal LEFT
-            LEFT = False
+            booleans_dict["left"] = False
         elif key == 115:
-            nonlocal DOWN
-            DOWN = False
+            booleans_dict["down"] = False
         elif key == 100:
-            nonlocal RIGHT
-            RIGHT = False
+            booleans_dict["right"] = False
 
     def poke(loc):
         """
@@ -295,15 +306,14 @@ def game():
             poke_mag = 100000 / distance**2
             if poke_mag > 200: #Limit magnitude of poke
                 poke_mag = 200
-            #Update velocity according to magnitude and direction
+            #update velocity according to magnitude and direction
             center.velocity += poke_mag * difference/distance
 
     #Game constants-----------------------------------------------------------
-    BACKGROUND_COLOR = (63, 63, 63)
-    #WINDOW_WIDTH x WINDOW_HEIGHT should be at least 670 x 325 to see help
-    #drawn properly.
-    WINDOW_WIDTH = WINDOW_HEIGHT = 800
-    WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    #window dimensions should be at least 670 x 325 for help_menu to be drawn
+    #properly.
+    window_dim = array([800.0, 800.0])
+    window = pygame.display.set_mode(window_dim.astype(int))
     help_text = ["left-click to poke the centers",\
                  "right-click to create a new center",\
                  "w,a,s,d moves the color center",\
@@ -317,9 +327,6 @@ def game():
                  "o   -- Toggle outline of Voronoi cells",\
                  "h   -- Toggle showing centers of Voronoi cells",\
                  "up  -- Cycle through color palettes"]
-    myfont = pygame.freetype.Font('NotoSansMono-Regular.ttf', 20)
-    help_text = [myfont.render(text, (255, 255, 255)) for text in help_text]
-    help_text = [i for i,_ in help_text] #Throw away the rect-like tuples
     #If adding to palettes, remember input: output will be 3-tuples with each
     #element an integer between 0 and 255.
     palettes = [lambda color: color,\
@@ -330,7 +337,7 @@ def game():
                 lambda color: (color[0], color[0], color[2]),\
                 lambda color: (155, 100, color[0])]
     number_of_centers = 50 #Initial number of cell centers
-    KEYDOWN_dict = {114: reset,          #'r'
+    keydown_dict = {114: reset,          #'r'
                     104: toggle_centers, #'h'
                     98: toggle_bouncing, #'b'
                     102: toggle_fill,    #'f'
@@ -338,28 +345,35 @@ def game():
                     273: next_palette,   #'up'
                     27: toggle_help,}    #'esc'
     clock = pygame.time.Clock() #For limiting fps
-    #Constants used for move_centers()
-    MAX_VEL = 15.0 #Max Velocity
-    OOB_DIM = array([WINDOW_WIDTH, WINDOW_HEIGHT]).astype(float) #Out-of-bounds
-
-    #Game Variables-----------------------------------------------------------
-    centers = {}; reset() #Create and randomly place cell centers
+    max_vel = 15.0 #Max Velocity
+    centers = {}
+    reset() #Randomly place cell centers
     #-----------------------------------------------------------------------
     #The color center is controlled with w,a,s,d. The distance from the color
     #center to the centers of voronoi cells determines those cells colors.
     #-----------------------------------------------------------------------
-    color_center = Center(array([WINDOW_WIDTH/2, WINDOW_HEIGHT/2]),\
-                          array([0.0, 0.0]))
-    PALETTE = palettes[0]
-    BOUNCING = FILL = OUTLINE = HELP = running = True
-    CENTERS_VISIBLE = UP = DOWN = LEFT = RIGHT = False
-
+    color_center = Center(window_dim/2, array([0.0, 0.0]))
+    palette = palettes[0]
+    booleans_dict = {"bouncing" : True,
+                     "fill": True,
+                     "outline" : True,
+                     "show_help" : True,
+                     "running" : True,
+                     "centers_visible" : False,
+                     "up" : False,
+                     "down" : False,
+                     "left" : False,
+                     "right" : False}
+    render_help()
     #Main Loop----------------------------------------------------------------
-    while running:
-        WINDOW.fill(BACKGROUND_COLOR)
-        if OUTLINE or FILL: draw_voronoi_cells()
-        if CENTERS_VISIBLE: draw_centers()
-        if HELP: draw_help()
+    while booleans_dict["running"]:
+        window.fill((63, 63, 63))
+        if booleans_dict["outline"] or booleans_dict["fill"]:
+            draw_voronoi_cells()
+        if booleans_dict["centers_visible"]:
+            draw_centers()
+        if booleans_dict["show_help"]:
+            draw_help()
         clock.tick(40)  #Limit frames per second (Comment out if you'd like)
         pygame.display.update()
         get_user_input()
@@ -367,6 +381,9 @@ def game():
     pygame.quit()
 
 def main():
+    """
+    Starts the game.
+    """
     pygame.init()
     pygame.display.set_caption('Soap')
     game()
